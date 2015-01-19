08a39bad492b157a528dfba619ffd0875d340cc0
unknown <zhaoyuxiang@zhaoyuxiang-L2.corp.qihoo.net>
2015-01-19 17:22:43 +0800

    add favicon

--- rec_played.js
+++ rec_played.js
@@ -1,11 +1,13 @@
 (function(){
     var countPlayed = 0;
+    var startTime = new Date();
     var recPlayed = $("#rec_played").text();
     setInterval(function(){
         var recPlayedNow = $("#rec_played").text();
         if(recPlayed !== recPlayedNow){
             ++countPlayed;
-            console.log("countPlayed is : " + countPlayed +" , and recPlayed is : " + recPlayedNow);
+            console.log("收听了 : " + countPlayed +" , 累积播放了 : " + recPlayedNow +
+            " , 现在是 : " + new Date() + " , 本次已经听了 : " + Math.floor((new Date()-a)/1000/60) + " 分钟");
             recPlayed = recPlayedNow;
         }
     },1000*30);
