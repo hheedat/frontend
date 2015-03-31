f1371af653184e2b4af832b6020e85e249ea39b5
hheedat <yuxiangzhao@outlook.com>
2015-03-31 19:22:50 +0800

    add func

--- func_colection.js
+++ func_colection.js
@@ -53,6 +53,12 @@ function addEvent(target,type,handler){
 		});
 	}
 }
+//removeEvent
+function removeEvent(target, type, handler){
+    target.removeEventListener ? target.removeEventListener(type, handler, false) : target.detachEvent("on" + type,  function(event){
+        return handler.call(target,event);
+    });
+}
 //cancelHandler
 function prevent(event){
 	var event = event || window.event;
@@ -60,4 +66,58 @@ function prevent(event){
 	if(event.returnValue) event.returnValue = false;
 	return false;
 }
-//
+//stopBubble
+function stopBubble(event) {
+    if ( event && event.stopPropagation ) event.stopPropagation();
+    else window.event.cancelBubble = true;
+}
+
+//handle URL
+function getQueryStringSearch(){
+    var qs = (location.search.length > 0 ? location.search.substring(1) : "");
+    var args = {};
+    var items = qs.length ? qs.split("&") : [];
+    var item = null;
+    var name = null;
+    var value = null;
+    for(var i = 0 , len = items.length ; i < len ; ++i){
+        item = items[i].split("=");
+        name = decodeURIComponent(item[0]);
+        value = decodeURIComponent(item[1]);
+        if(name.length){
+            args[name] = value;
+        }
+    }
+    return args;
+}
+
+//英文单词占1B，中文2B
+function getByteLen(val) { 
+    var len = 0; 
+    for (var i = 0; i < val.length; i++) { 
+        if (val[i].match(/[^x00-xff]/ig) != null) //全角 
+            len += 2; 
+        else
+            len += 1; 
+    }; 
+    return len; 
+} 
+
+// function getCookie(name){
+//     if (document.cookie.length>0){
+//         var start=document.cookie.indexOf(name + "=");
+//         if (start!=-1){ 
+//             start=start + name.length+1 ;
+//             var end=document.cookie.indexOf(";",start);
+//             if (end==-1) end=document.cookie.length;
+//             return decodeURI(document.cookie.substring(start,end));
+//         } 
+//     }
+//     return "";
+// }
+
+// function setCookie(name,value,expiredays){
+//     var exdate=new Date();
+//     exdate.setDate(exdate.getDate()+expiredays);
+//     document.cookie=name+ "=" +encodeURI(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+// }
