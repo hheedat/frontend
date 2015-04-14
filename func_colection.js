6155a622160fa8fc1f3361f1dcee53bc668d67c7
zhaoyuxiang <yuxiangzhao@outlook.com>
2015-04-15 01:11:28 +0800

    add onReady functions

--- func_colection.js
+++ func_colection.js
@@ -52,6 +52,29 @@ function onReady(fn) {
         window.addEventListener('DOMContentLoaded', fn);
     }
 }
+function onReady(fn) {
+    var readyState = document.readyState;
+    if (readyState === 'interactive' || readyState === 'complete') {
+        setTimeout(fn, 0);
+    } else {
+        window.addEventListener('DOMContentLoaded', fn);
+    }
+}
+function onReadyPromise() {
+    return new Promise(function (resolve, reject) {
+        var readyState = document.readyState;
+        if (readyState === 'interactive' || readyState === 'complete') {
+            resolve();
+        } else {
+            window.addEventListener('DOMContentLoaded', resolve);
+        }
+    });
+}
+onReadyPromise().then(function () {
+    console.log('DOM fully loaded and parsed');
+});
+console.log('==Starting==');
+
 
 //addEvent
 function addEvent(target,type,handler){
