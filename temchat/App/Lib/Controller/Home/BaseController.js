859ca01ffb0004404d0a5aeef976e8db5b9c13a9
hheedat <yuxiangzhao@outlook.com>
2015-04-23 21:30:35 +0800

    more code

--- temchat/App/Lib/Controller/Home/BaseController.js
+++ temchat/App/Lib/Controller/Home/BaseController.js
@@ -10,6 +10,23 @@ module.exports = Controller(function() {
 		init: function(http) {
 			this.super("init", http);
 			//其他的通用逻辑
+            var self = this;
+			if(http.action === 'login' || http.action === 'register'){
+                return ;
+            }else{
+                return self.session('userInfo').then(function(data){
+                    if(!isEmpty(data)){
+                        self.userInfo = data;
+                    }else{
+                        if(self.action !== 'index'){
+                            self.assign({
+                               'info':'您还没有登录'
+                            });
+                            self.display('index');
+                        }
+                    }
+                });
+            }
 		}
 	}
 })
--- temchat/App/Lib/Controller/Home/IndexController.js
+++ temchat/App/Lib/Controller/Home/IndexController.js
@@ -7,16 +7,33 @@ module.exports = Controller("Home/BaseController", function() {
 	return {
 		indexAction: function() {
 			//render View/Home/index_index.html file
-			this.display();
+            var self = this;
+            return self.session('userInfo').then(function(data){
+                if(isEmpty(data)) {
+                    return self.display();
+                }else{
+                    self.assign({
+                        'islogin':true,
+                        'name':data.name
+                    })
+                    return self.display();
+                }
+            });
 		},
+        logoutAction:function(){
+            var self = this;
+            return self.session().then(function(){
+                return self.redirect('index');
+            });
+        },
 		loginAction: function() {
 			var self = this;
 
 			//页面post
 			if (self.isPost()) {
                 var email = self.post('email');
-				var name = self.post('name'); //获取post过来的用户名
-				var pwd = self.post('pwd'); //获取post过来的密码
+				var pwd = self.post('pwd');
+                var name = null;
 
 				return D('User').where({ //根据用户名和密码查询符合条件的数据
 					email : email,
@@ -31,13 +48,16 @@ module.exports = Controller("Home/BaseController", function() {
 						return self.display();
 					} else {
                         //用户登录成功写入Session
+                        name = data.name;
 						return self.session('userInfo', data);
 					}
 				}).then(function() {
-                    self.assign({
-                        'info':"登录成功"
-                    });
-					return self.display('index');
+                    //self.assign({
+                    //    'islogin':true,
+                    //    'name':name
+                    //})
+                    //return self.display('index');
+                    return self.redirect('index');
 				});
 			} else {
 				self.assign({
