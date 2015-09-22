9a0244da34af76949729eb5f6090b13d6b93cf0f
hheedat <yuxiangzhao@outlook.com>
2015-09-22 19:25:42 +0800

    more practices

--- learn_shell/3.sh
+++ learn_shell/3.sh
@@ -5,3 +5,17 @@ echo $name
 
 name="Jack"
 echo $name
+
+echo "hello , my name is \"$name\""
+
+age=23
+
+echo $name $age
+
+echo $name "length:" ${#name}
+
+echo $name "1-3:" ${name:1:2}
+
+str="how are you ? I'm fine."
+
+echo `expr index "$str" w`
