c2a541f4011f77d74a512733963369a194e0138e
zhaoyuxiang <yuxiangzhao@outlook.com>
2015-12-11 01:42:09 +0800

    add promise demo

---
+++ promise/promise_2_1.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+function helloPromise(filePath){
+    return new Promise(function(resolve,reject){
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            if (err) return reject(err);
+            console.log(data);
+            resolve();
+        });
+    });
+}
+
+helloPromise('1.txt').then(function(){
+    return helloPromise('2.txt');
+}).then(function(){
+    return helloPromise('4.txt');
+}).catch(function(err){
+    console.log('出错了');
+    console.error(err);
+}).then(function(){
+    return helloPromise('3.txt');
+});
+
+console.log('开始');
