e33e2d3205f24d90f648bb1e9411c2c78bc95866
hheedat <yuxiangzhao@outlook.com>
2016-05-11 20:14:40 +0800

    more demoes

---
+++ learn_go/33_stringer.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+type IPAddr [4]int
+
+func (i IPAddr) String() string {
+	var tmp []string
+
+	for _, v := range i {
+		tmp = append(tmp, strconv.Itoa(v))
+	}
+
+	return strings.Join(tmp, ",")
+}
+
+func main() {
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphod Beeblebrox", 9001}
+	fmt.Println(a, z)
+
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
