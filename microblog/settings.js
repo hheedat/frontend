d43736bf8d83707a4259ac71f99258521e9b73a8
hheedat <yuxiangzhao@outlook.com>
2016-06-04 15:42:51 +0800

    delete something

--- microblog/settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	cookie_secret : 'hheedat',
-	db : 'microblog',
-	host : 'localhost',
-	port : 27017
-}
--- temchat/App/Lib/Controller/Home/chatController.js
+++
@@ -1,8 +0,0 @@
-module.exports = Controller("Home/BaseController",function(){
-    "use strict";
-    return {
-        indexAction:function(){
-            console.log("bananananananananana")
-        }
-    }
-});
