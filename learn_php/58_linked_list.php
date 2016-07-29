8055caad486cc5f1212d10c69ccc11fa90d31280
zhaoyuxiang <yuxiangzhao@outlook.com>
2016-07-30 00:36:14 +0800

    practice

--- learn_php/58_linked_list.php
+++ learn_php/58_linked_list.php
@@ -40,7 +40,7 @@ class LinkedList
     {
         if ($this->_head == null) {
             return false;
-        } else {
+        } elseif ($this->_length > 1) {
             $node = $this->makeNode($this->_tail->data);
 
             $secondTail = $this->_head;
@@ -52,12 +52,17 @@ class LinkedList
             $this->_tail->next = null;
             --$this->_length;
             return $node;
+        } elseif ($this->_length == 1) {
+            $node = $this->makeNode($this->_tail->data);
+            $this->_head = $this->_tail = null;
+            --$this->_length;
+            return $node;
         }
     }
 
     function unshift($node)
     {
-        if ($this->_head == NULL) {
+        if ($this->_head == null) {
             $this->_head = $this->_tail = $node;
         } else {
             $node->next = $this->_head;
@@ -90,6 +95,19 @@ class LinkedList
 
     function reverse()
     {
+        if ($this->_length > 1) {
+            $this->_tail = $p = $this->_head;
+            if ($p != null) $q = $p->next;
+            if ($q != null) $r = $q->next;
+            while ($q != null) {
+                $q->next = $p;
+                $p = $q;
+                $q = $r;
+                if ($r != null) $r = $r->next;
+            }
+            $this->_head = $p;
+            $this->_tail->next = null;
+        }
     }
 
     function getLength()
@@ -118,6 +136,12 @@ echo "shift , value is : " . $linkedList->shift()->data . "\n";
 echo "pop , value is : " . $linkedList->pop()->data . "\n";
 
 echo "linked list length is " . $linkedList->getLength() . " \n";
+$linkedList->map(function ($val, $index) {
+    echo "index is : $index \t value is : $val \n";
+});
+
+$linkedList->reverse();
+echo "linked list length is " . $linkedList->getLength() . " after reverse\n";
 $linkedList->map(function ($val, $index) {
     echo "index is : $index \t value is : $val \n";
 });
