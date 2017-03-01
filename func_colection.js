73a45514e3b716b3851061114250212c770c08b4
hheedat <yuxiangzhao@outlook.com>
2017-03-01 11:24:25 +0800

    add function

--- func_colection.js
+++ func_colection.js
@@ -184,3 +184,15 @@ function formatStr2(str){
 }
 console.log(formatStr(str));
 console.log(formatStr2(str));
+
+//结尾是否出现了循环模式
+function f(str){
+    var res = str.match(/(([01]+?)[01]*?)\1+\2$/);
+    return {
+        src: str,
+        tail: res[0],
+        sub: res[1],
+        len: res[0].length / res[1].length
+    }
+}
+console.log(f('11111111101001001001001001'));
